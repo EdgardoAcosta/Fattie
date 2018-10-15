012f4038c7438247c638819e8810770eafcdb406
Edgar Rodriguez <edgar.rodriguez.turrubiates@gmail.com>
2018-10-14 23:26:15 -0500

    #cambio en el parser y scanner ya aceptar los var y main, sigue marcando unexpected NEW_LINE

--- fattie/cube.py
+++ fattie/cube.py
@@ -72,11 +72,61 @@ cube.set_cube(Operator.EQUAL, Types.FLOAT, Types.FLOAT, Types.FLOAT)
 cube.set_cube(Operator.EQUAL, Types.BOOLEAN, Types.BOOLEAN, Types.BOOLEAN)
 cube.set_cube(Operator.EQUAL, Types.CHAR, Types.CHAR, Types.CHAR)
 
+#SpecialFunctions
 
-# cube.set_cube(SpecialFunction.INPUT, Types.CHAR, Types.CHAR, Types.CHAR)
-
-
-
+# Input
+cube.set_cube(SpecialFunction.INPUT, Types.CHAR, None, Types.CHAR)
+cube.set_cube(SpecialFunction.INPUT, Types.INT, None, Types.INT)
+cube.set_cube(SpecialFunction.INPUT, Types.FLOAT, None, Types.FLOAT)
+#Print
+cube.set_cube(SpecialFunction.PRINT, Types.INT, None, None)
+cube.set_cube(SpecialFunction.PRINT, Types.CHAR, None, None)
+cube.set_cube(SpecialFunction.PRINT, Types.FLOAT, None, None)
+#MoveUp
+cube.set_cube(SpecialFunction.MOVEUP, Types.INT, None, None)
+cube.set_cube(SpecialFunction.MOVEUP, Types.CHAR, None, None)
+cube.set_cube(SpecialFunction.MOVEUP, Types.FLOAT, None, None)
+#MoveDown
+cube.set_cube(SpecialFunction.MOVEDOWN, Types.INT, None, None)
+cube.set_cube(SpecialFunction.MOVEDOWN, Types.CHAR, None, None)
+cube.set_cube(SpecialFunction.MOVEDOWN, Types.FLOAT, None, None)
+#MoveRight
+cube.set_cube(SpecialFunction.MOVERIGHT, Types.INT, None, None)
+cube.set_cube(SpecialFunction.MOVERIGHT, Types.CHAR, None, None)
+cube.set_cube(SpecialFunction.MOVERIGHT, Types.FLOAT, None, None)
+#MoveLeft
+cube.set_cube(SpecialFunction.MOVELEFT, Types.INT, None, None)
+cube.set_cube(SpecialFunction.MOVELEFT, Types.CHAR, None, None)
+cube.set_cube(SpecialFunction.MOVELEFT, Types.FLOAT, None, None)
+#Angle
+cube.set_cube(SpecialFunction.ANGLE, Types.INT, None, None)
+#Color
+cube.set_cube(SpecialFunction.COLOR, Types.INT, None, Types.CHAR)
+#Circle
+cube.set_cube(SpecialFunction.CIRCLE, Types.INT, None, None)
+#Square
+cube.set_cube(SpecialFunction.SQUARE, Types.INT, None, None)
+#Clean
+cube.set_cube(SpecialFunction.CLEAN, Types.INT, None, None)
+cube.set_cube(SpecialFunction.CLEAN, Types.FLOAT, None, None)
+#Draw
+cube.set_cube(SpecialFunction.DRAW, Types.INT, None, None)
+cube.set_cube(SpecialFunction.DRAW, Types.FLOAT, None, None)
+#StartPosition<--------------- Checar lo de la doble atributo de la funcion
+cube.set_cube(SpecialFunction.STARTPOSITION, Types.INT, Types.INT, None)
+cube.set_cube(SpecialFunction.STARTPOSITION, Types.FLOAT, Types.FLOAT, None)
+#ScreenSizeX
+cube.set_cube(SpecialFunction.SCREENSIZESX, Types.INT, None, Types.INT)
+#ScreenSizeY
+cube.set_cube(SpecialFunction.SCREENSIZESY, Types.INT, None, Types.INT)
+#GO change of position with out painting
+cube.set_cube(SpecialFunction.GO, Types.INT, None, None)
+#Fibonacci
+cube.set_cube(SpecialFunction.FIBONACCI, Types.INT, None, None)
+#Factorial
+cube.set_cube(SpecialFunction.FACTORIAL, Types.INT, None, None)
+#Sleep sleep de funcition N miliseconds
+cube.set_cube(SpecialFunction.SLEEP, Types.INT, None, None)
 
 print(cube.__dict__)
 
